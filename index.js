f50ef9a02cf3322a616d106ea26af02f1cc01a01
Stefan Bozov <sbozov@pirinsoft.bg>
2020-01-21 14:51:10 +0200

    implement global default restore logic

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const readFile = util.promisify(fs.readFile);
 const bodyParser = require('body-parser');
 const port = process.env.SERVER_PORT || 8004;
 const layoutsFolder = process.env.LAYOUTS_FOLDER || './layouts';
+const defaultFolder = process.env.LAYOUTS_FOLDER || './default';
 
 
 app.use(bodyParser.json());
@@ -16,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/layouts', (req, res, next) => {
     const user = getUser(req);
-    fetchConfigurations(user).then(layouts => {
+    fetchConfigurations(layoutsFolder, user).then(layouts => {
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify({ layouts }));
     }).catch(err => next(err));
@@ -43,16 +44,35 @@ app.delete('/layouts', (req, res, next) => {
     res.send();
 });
 
+app.get('/default', (req, res, next) => {
+    const user = getUser(req);
+    const file = path.join(defaultFolder, "layout.json");
+    readFile(file, 'utf8').then((info) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(info);
+    }).catch(err => next(err));
+});
+
+app.post('/default', (req, res, next) => {
+    const user = getUser(req);
+    const name = req.body.name;
+    const file = path.join(defaultFolder, "layout.json");
+    fs.writeFileSync(file, JSON.stringify({ name }), "utf8");
+    console.log(`select default global layout ${name} for user ${user}`);
+    res.status(201);
+    res.send();
+});
+
 app.listen(port,
     () => console.log(`Glue42 demo layouts server listening on ${port}. Will load layouts from ${layoutsFolder} folder`));
 
 
-async function fetchConfigurations(user) {
+async function fetchConfigurations(folder, user) {
 
     console.log(`fetching layouts for ${user}`);
 
-    return readDir(layoutsFolder)
-        .then(files => files.map(fn => readFile(path.join(layoutsFolder, fn), 'utf8').then(json.parse)))
+    return readDir(folder)
+        .then(files => files.map(fn => readFile(path.join(folder, fn), 'utf8').then(json.parse)))
         .then(p => Promise.all(p));
 }
 
